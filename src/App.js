9fefde6d937bfaef8001fbd46ab2a75e8c79b749
livujjwal <ujjwalsingh2112@gamil.com>
2023-10-21 19:01:41 +0530

    struture

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DisplayMovies from "./Components/DisplayMovies";
+import MoviesDetails from "./Components/MoviesDetails";
+import data from "./Data/moviesData";
+function App(){
+return (<div>
+    <DisplayMovies/>
+</div>)
+}
+
+export default App;
---
+++ src/Components/MoviesDetails.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function MoviesDetails() {
+    
+}
+export default MoviesDetails
